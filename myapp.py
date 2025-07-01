9c497532413a6cd5b7c117d774852286e5093e4a
santosh thakur <santosh990th32@gmail.com>
2025-07-01 10:13:55 +0530

    मूड ट्रैकिंग बेसिक कोड फाइनल किया

--- myapp.py
+++ myapp.py
@@ -1,4 +1,12 @@
-print("This is a new line for my app!")
-print("Hello, this is my mental health app prototype!")
-print("This is a new line for my app!")
 
+
+from datetime import datetime
+
+def log_mood():
+    mood = input("आज आपका मूड कैसा है? (खुश/उदास/ठीक):")
+    with open("mood_log.txt", "a") as f:
+        f.write(f"{mood} - {datetime.now()}\n")
+    print("मूड लॉग हो गया!")
+
+print("Welcome to Mental Health App Prototype!")
+log_mood()
